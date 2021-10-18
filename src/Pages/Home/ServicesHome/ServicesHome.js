50e04e0c9ba8f9ec13f8117aa62b8d913368eee5
smahkafi <sakafi.m@gmail.com>
2021-10-18 09:43:40 +0600

    service home header add

--- src/Pages/Home/ServicesHome/ServicesHome.js
+++ src/Pages/Home/ServicesHome/ServicesHome.js
@@ -3,10 +3,12 @@ import React from 'react';
 const ServicesHome = () => {
     return (
         <div>
+            <div>
+                <h2>Our Departments</h2>
+                <p>Esteem spirit temper too say adieus who direct esteem.It esteems luckily or picture placing drawing.</p>
+            </div>
             <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+            
         </div>
     );
 };
