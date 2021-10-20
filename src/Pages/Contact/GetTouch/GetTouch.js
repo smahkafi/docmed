4b1d9af0ecf72ce4a8e3fd252fbed642fc9b23c0
smahkafi <sakafi.m@gmail.com>
2021-10-20 21:53:57 +0600

    contact form button margin set

--- src/Pages/Contact/GetTouch/GetTouch.js
+++ src/Pages/Contact/GetTouch/GetTouch.js
@@ -10,12 +10,12 @@ const GetTouch = () => {
             <div className="row mt-5">
                 <h2>Get in Touch</h2>
                 <div className="col-sm-12 col-md-8 col-lg-8 ">
-                <textarea class=" w-100" name="message" id="message" cols="30" rows="9" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Message'" placeholder="Enter Message"></textarea>
+                <textarea class=" w-100" name="message" id="message" cols="30" rows="9" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Message'" placeholder="Enter Message"></textarea><br />
                     <br />
-                    <input class="form-control valid" name="name" id="name" type="text" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your name'" placeholder="Enter your name"/>
+                    <input class="form-control valid" name="name" id="name" type="text" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your name'" placeholder="Enter your name"/><br />
                     <input class="form-control valid" name="email" id="email" type="email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'" placeholder="Email"/>
                     <br />
-                    <input class="form-control" name="subject" id="subject" type="text" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Subject'" placeholder="Enter Subject"/>
+                    <input class="form-control" name="subject" id="subject" type="text" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Subject'" placeholder="Enter Subject"/> <br />
                     <Button type="submit" class="button button-contactForm boxed-btn m-5">Send</Button>
                 </div>
                 <div className="col-sm-12 col-md-4 col-lg-4 align-self-center text-start">
