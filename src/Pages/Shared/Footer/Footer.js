e2a33820b5ba0cad7ae93762ba1d02c598017c64
smahkafi <sakafi.m@gmail.com>
2021-11-27 23:23:32 +0600

    origin changing

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -4,7 +4,6 @@ import { Container } from 'react-bootstrap';
 import { faFacebookSquare, faTwitterSquare, faYoutubeSquare } from '@fortawesome/free-brands-svg-icons';
 import { Link } from 'react-router-dom';
 
-
 const Footer = () => {
     return (
         <div className="bg-secondary">
